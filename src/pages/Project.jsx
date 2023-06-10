49f7c64326be7520b605dc53f7ff41c534357324
Koushikxy <ksmaity21@gmail.com>
2023-06-11 02:44:34 +0530

    new project

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -4,6 +4,10 @@ import Footer from '../components/Footer'
 
 import { Link } from 'react-router-dom'
 
+
+
+import apnafashion from '../projects_img/apnafashion.png'
+import portfolio from '../projects_img/Potfolio website.png'
 import pdftoword from '../projects_img/pdf to word converter.png'
 import wordandcharcounter from '../projects_img/word and character counter.png'
 import qrcodescanner from '../projects_img/QR code Scanner.png'
@@ -16,6 +20,27 @@ const project = () => {
       <div className='bg-[#1D1D1F] h-fit w-full scroll-smooth'>
         <div className='lg:pt-52 lg:px-40 lg:pb-20 md:pt-40 md:px-18 md:pb-12 pt-36 px-6 pb-14'>
           <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1'>
+            <div data-aos='fade-up' data-aos-delay='100' data-aos-once='false' className='m-2 border-2 border-slate-300 text-slate-300 hover:border-[#E79418] rounded-md p-2'>
+              <div className='lg:h-[50vh] md:h-[40vh] h-[55vh]'>
+                <img className='hover:border-2 hover:border-[#E79418] rounded-md' src={apnafashion} alt="react certificate" />
+                <h1 className='font-semibold tracking-[1px] text-center text-2xl my-4 border-y-2 p-1 text-[#E79418] hover:border-[#E79418]'>APNA FASHION</h1>
+                <p className='text-center font-medium tracking-[1px]'>It is a Clothing E-Commerce website using FIgma, Canva, React.js, Redux and Tailwind</p>
+              </div>
+              <div className='flex justify-center mb-2'>
+                <button className='text-slate-300 border-2 py-2 px-[5.6vw] rounded-md font-medium text-sm tracking-[1px] hover:text-black hover:bg-white focus:outline-none opacity-40' disabled>UPCOMING</button>
+              </div>
+            </div>
+            <div data-aos='fade-up' data-aos-delay='100' data-aos-once='false' className='m-2 border-2 border-slate-300 text-slate-300 hover:border-[#E79418] rounded-md p-2'>
+              <div className='lg:h-[50vh] md:h-[40vh] h-[55vh]'>
+                <img className='hover:border-2 hover:border-[#E79418] rounded-md' src={portfolio} alt="react certificate" />
+                <h1 className='font-semibold tracking-[1px] text-center text-2xl my-4 border-y-2 p-1 text-[#E79418] hover:border-[#E79418]'>PORTFOLIO WEBSITE</h1>
+                <p className='text-center font-medium tracking-[1px]'>My Portfolio website using Figma, React.js and Tailwind CSS</p>
+              </div>
+              <div className='flex justify-around mb-2'>
+                <button className='text-slate-300 border-2 p-2  rounded-md font-medium text-sm tracking-[1px] hover:text-black hover:bg-white'><Link target='_blank' to='https://github.com/koushikxy/koushikMaity'>SOURCE CODE</Link></button>
+                <button className='text-slate-300 border-2 py-2 px-5 rounded-md font-medium text-sm tracking-[1px] hover:text-black hover:bg-white'><Link target='_blank' to='https://koushikmaity.netlify.app/'>LIVE VIEW</Link></button>
+              </div>
+            </div>
             <div data-aos='fade-up' data-aos-delay='100' data-aos-once='false' className='m-2 border-2 border-slate-300 text-slate-300 hover:border-[#E79418] rounded-md p-2'>
               <div className='lg:h-[50vh] md:h-[40vh] h-[55vh]'>
                 <img className='hover:border-2 hover:border-[#E79418] rounded-md' src={pdftoword} alt="react certificate" />
